6a5541c716029d00cd36d4ec5a175ec97c6b9acb
Siman <wangsiman-carol@outlook.com>
2021-01-29 17:44:16 -0800

    Initial Commit

---
+++ EmployeeList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmployeeList
+//
+//  Created by Wang Siman on 1/29/21.
+//  Copyright © 2021 None. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
